c57632c064eb5f4ca1907aa0c2385c81def9f1d1
math92n4 <112948957+math92n4@users.noreply.github.com>
2023-02-06 12:45:32 +0000

    done

---
+++ fix.samples.sh
@@ -0,0 +1,10 @@
+cd /workspaces/bbs-math92n4/samples/movies
+cat Sex.txt And.txt The.txt City.txt > ./"Sex and the City.txt"
+rm -r Sex.txt
+rm -r And.txt
+rm -r The.txt
+rm -r City.txt
+cd /workspaces/bbs-math92n4/samples/animals
+cat Elephant.txt babies.txt > ./"Elephant babies.txt"
+rm -r Elephant.txt
+rm -r babies.txt
